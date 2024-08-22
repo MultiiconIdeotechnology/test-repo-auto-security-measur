67b822c29f3932f35818682c0f6c1d1b73434a00
technical15 <technical15.multiicon@gmail.com>
2024-08-22 09:17:11 +0530

    changes in travel saved filter binding --keyur

--- src/app/modules/crm/collection/travel/travel.component.ts
+++ src/app/modules/crm/collection/travel/travel.component.ts
@@ -139,6 +139,12 @@ export class TravelCollectionComponent extends BaseListingComponent {
 
                 let filterData = JSON.parse(this._filterService.activeFiltData.grid_config);
                 this.selectedAgent = filterData['table_config']['agencyName']?.value;
+                if(this.selectedAgent && this.selectedAgent?.id){
+                    const match = this.agentList.find((item: any) => item.id == this.selectedAgent?.id);
+                    if (!match) {
+                        this.agentList.push(this.selectedAgent);
+                    }
+                }
               
                 if (filterData['table_config']['dueDate'].value) {
                     filterData['table_config']['dueDate'].value = new Date(filterData['table_config']['dueDate'].value);
@@ -179,13 +185,6 @@ export class TravelCollectionComponent extends BaseListingComponent {
     getAgent(value: string) {
         this.agentService.getAgentComboMaster(value, true).subscribe((data) => {
             this.agentList = data;
-
-            if(this.selectedAgent && this.selectedAgent?.id){
-                const match = this.agentList.find((item: any) => item.id == this.selectedAgent?.id);
-                if (!match) {
-                    this.agentList.push(this.selectedAgent);
-                }
-            }
 
             for (let i in this.agentList) {
                 this.agentList[i]['agent_info'] = `${this.agentList[i].code}-${this.agentList[i].agency_name}${this.agentList[i].email_address}`;
