ecbb703f7e12675c3c13c19feb350b9457f53db7
MultiiconTechnical13 <technical13.multiicon@gmail.com>
2024-11-26 18:01:59 +0530

    Update

--- src/app/modules/crm/lead/inbox/inbox.component.ts
+++ src/app/modules/crm/lead/inbox/inbox.component.ts
@@ -1,5 +1,5 @@
 import { NgIf, NgFor, NgClass, DatePipe, AsyncPipe, CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
@@ -23,8 +23,6 @@ import { FuseConfirmationService } from '@fuse/services/confirmation';
 import { AppConfig } from 'app/config/app-config';
 import { Security, filter_module_name, leadPermissions, messages, module_name } from 'app/security';
 import { CrmService } from 'app/services/crm.service';
-import { ToasterService } from 'app/services/toaster.service';
-import { GridUtils } from 'app/utils/grid/gridUtils';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { Subject, takeUntil } from 'rxjs';
 import { MarketingMaterialsComponent } from '../marketing-materials/marketing-materials.component';
@@ -32,7 +30,7 @@ import { DialCallListComponent } from '../dial-call-list/dial-call-list.componen
 import { CRMScheduleCallListComponent } from '../schedule-call-list/schedule-call-list.component';
 import { EntityService } from 'app/services/entity.service';
 import { LeadStatusChangedLogComponent } from '../lead-status-changed-log/lead-status-changed-log.component';
-import { BaseListingComponent } from 'app/form-models/base-listing';
+import { BaseListingComponent, Column } from 'app/form-models/base-listing';
 import { PrimeNgImportsModule } from 'app/_model/imports_primeng/imports';
 import { Subscription } from 'rxjs';
 import { CommonFilterService } from 'app/core/common-filter/common-filter.service';
@@ -77,6 +75,7 @@ import { ScheduleCallRemarkComponent } from '../call-history-tab/schedule-call-d
 
 export class InboxComponent extends BaseListingComponent {
     @Input() isFilterShowInbox: boolean;
+    @Input() selectedColumns: Column[];
     @Output() isFilterShowInboxChange = new EventEmitter<boolean>();
     @ViewChild('tabGroup') tabGroup;
     @ViewChild(MatPaginator) public _paginatorInbox: MatPaginator;
@@ -108,7 +107,7 @@ export class InboxComponent extends BaseListingComponent {
         private conformationService: FuseConfirmationService,
         private matDialog: MatDialog,
         private entityService: EntityService,
-        public _filterService: CommonFilterService
+        public _filterService: CommonFilterService,
     ) {
         super(module_name.lead);
         this.key = this.module_name;
@@ -134,6 +133,13 @@ export class InboxComponent extends BaseListingComponent {
             this.isFilterShowInboxChange.emit(this.isFilterShowInbox);
             this.primengTable._filter();
         });
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log("changes", changes);
+        //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+        //Add '${implements OnChanges}' to the class.
+        
     }
 
     ngAfterViewInit() {
