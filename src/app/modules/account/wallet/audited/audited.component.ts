7e4d76de4c1e5a31afaf0195148c4f26930d2cf3
technical15 <technical15.multiicon@gmail.com>
2025-01-28 16:08:50 +0530

    from date to date changes --keyur

--- src/app/modules/account/wallet/audited/audited.component.ts
+++ src/app/modules/account/wallet/audited/audited.component.ts
@@ -335,8 +335,8 @@ export class AuditedComponent extends BaseListingComponent {
     filterReq['particularId'] = this.auditListFilter?.particularId == "all" ? '' : this.auditListFilter?.particularId;
     filterReq['mop'] = this.auditListFilter?.mop || '';
     filterReq['psp'] = this.auditListFilter?.psp || '';
-    filterReq['FromDate'] = DateTime.fromJSDate(new Date(this.auditListFilter.FromDate)).toFormat('yyyy-MM-dd');
-    filterReq['ToDate'] = DateTime.fromJSDate(new Date(this.auditListFilter.ToDate)).toFormat('yyyy-MM-dd');
+    // filterReq['FromDate'] = DateTime.fromJSDate(new Date(this.auditListFilter.FromDate)).toFormat('yyyy-MM-dd');
+    // filterReq['ToDate'] = DateTime.fromJSDate(new Date(this.auditListFilter.ToDate)).toFormat('yyyy-MM-dd');
     filterReq['Take'] = this.totalRecords;
 
 
