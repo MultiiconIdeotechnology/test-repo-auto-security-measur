49225c1e8290d52269746eee58ec063932ebfe36
technical15 <technical15.multiicon@gmail.com>
2025-02-06 14:30:56 +0530

    sale return and sale book dmcc/agent column added --keyur

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
 
     // apiUrl: 'http://localhost:5041/api/',
-    apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
-    // apiUrl: 'https://boapi.bontontech.com/api/',
+    // apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
+    apiUrl: 'https://boapi.bontontech.com/api/',
     // apiUrl: 'https://2c04-2402-a00-182-32cd-dc0f-e2f5-edab-ef91.ngrok-free.app/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
