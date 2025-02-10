c5263e368ddb7a9bc519fa4f1f9f6d7b4565e786
MultiiconTechnical13 <technical13.multiicon@gmail.com>
2025-02-10 11:30:26 +0530

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
     production: false,
 
-    apiUrl: 'http://localhost:5041/api/',
-    // apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
-    // apiUrl: 'https://e90d-103-86-16-34.ngrok-free.app/api/',
-    // apiUrl: 'http://localhost:48046/api/',
+    // apiUrl: 'http://localhost:5041/api/',
+    apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
+    // apiUrl: 'https://boapi.bontontech.com/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
     isEnvironment: 'staging'
