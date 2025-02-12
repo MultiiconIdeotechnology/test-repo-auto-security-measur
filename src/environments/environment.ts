28c907864779cdfe9ab358938fd21d58584482f5
MultiiconDev3 <technical11.multiicon@gmail.com>
2025-02-12 19:11:52 +0530

    MIT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://boapi.bontontech.com/api/',
+    apiUrl: 'https://boapi.bontontech.com/api/',
 
-     apiUrl: 'http://localhost:5041/api/',
+    //  apiUrl: 'http://localhost:5041/api/',
     // apiUrl: 'http://localhost:48046/api/',
     // apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
