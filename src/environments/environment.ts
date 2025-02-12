7754a5b2c49668bf1161637c1902b3c10eee61f7
MultiiconTechnical10 <technical10.multiicon@gmail.com>
2025-02-12 16:56:45 +0530

    Airline Carrier Wise Analysis add bnt supplier add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
 
-    apiUrl: 'https://66b1-103-86-16-34.ngrok-free.app/api/',
+    // apiUrl: 'https://66b1-103-86-16-34.ngrok-free.app/api/',
     // apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
-    // apiUrl: 'https://boapi.bontontech.com/api/',
+    apiUrl: 'https://boapi.bontontech.com/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
     isEnvironment: 'staging'
