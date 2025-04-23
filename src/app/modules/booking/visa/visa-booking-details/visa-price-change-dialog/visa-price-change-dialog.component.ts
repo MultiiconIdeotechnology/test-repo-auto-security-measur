8e7300d40dc3842a48e06ff92deb6468118aeaba
technical15 <technical15.multiicon@gmail.com>
2025-04-23 11:54:30 +0530

    keyur | visa price dialog component added

---
+++ src/app/modules/booking/visa/visa-booking-details/visa-price-change-dialog/visa-price-change-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-visa-price-change-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './visa-price-change-dialog.component.html',
+  styleUrls: ['./visa-price-change-dialog.component.scss']
+})
+export class VisaPriceChangeDialogComponent {
+
+}
