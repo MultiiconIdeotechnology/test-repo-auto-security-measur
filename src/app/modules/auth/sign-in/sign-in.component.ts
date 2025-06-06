01988c132531f6fe5abd7e844da0948b5f254d60
technical15 <technical15.multiicon@gmail.com>
2025-06-06 18:21:15 +0530

    keyur | console removed

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -212,7 +212,6 @@ export class AuthSignInComponent implements OnInit {
                 const redirectURL = this._activatedRoute.snapshot.queryParamMap.get('redirectURL') || '/signed-in-redirect';
                 // Navigate to the redirect url
                 this._router.navigateByUrl(redirectURL);
-                console.log("extBody",  extBody.authtype)
 
                 // need to do
                 if (!extBody.authtype) {
