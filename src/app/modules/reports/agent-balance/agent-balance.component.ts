5688ed3ad5ed0f87853f6f64b5fc586c082b524b
MultiiconTechnical9 <technical9.multiicon@gmail.com>
2025-06-30 17:22:44 +0530

    --Nahin | Agent Balance Register dynamic column changes done

--- src/app/modules/reports/agent-balance/agent-balance.component.ts
+++ src/app/modules/reports/agent-balance/agent-balance.component.ts
@@ -18,7 +18,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterOutlet } from '@angular/router';
 import { FuseConfirmationService } from '@fuse/services/confirmation';
-import { BaseListingComponent } from 'app/form-models/base-listing';
+import { BaseListingComponent, Column, Types } from 'app/form-models/base-listing';
 import { Security, filter_module_name, messages, module_name } from 'app/security';
 import { Excel } from 'app/utils/export/excel';
 import { DateTime } from 'luxon';
@@ -29,6 +29,7 @@ import { AgentBalanceService } from 'app/services/agent-balance.service';
 import { RefferralService } from 'app/services/referral.service';
 import { Subscription } from 'rxjs';
 import { CommonFilterService } from 'app/core/common-filter/common-filter.service';
+import { cloneDeep } from 'lodash';
 
 
 @Component({
@@ -76,6 +77,12 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
     selectedRM: any;
     selectedAgent: any;
 
+    types = Types;
+    cols: Column[] = [];
+    selectedColumns: Column[] = [];
+    exportCol: Column[] = [];
+    activeFiltData: any = {};
+
     constructor(
         private agentBalanceService: AgentBalanceService,
         private agentService: AgentService,
@@ -89,6 +96,19 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
         this.sortDirection = 'desc';
         this.Mainmodule = this;
         this._filterService.applyDefaultFilter(this.filter_table_name);
+
+        this.selectedColumns = [
+            { field: 'agent_code', header: 'Agent Code', type: Types.number, isFrozen: false , fixVal:0 },
+            { field: 'agent_name', header: 'Agent', type: Types.select, isFrozen: false },
+            { field: 'balance', header: 'Balance', type: Types.number, fixVal: 2, class:'text-right'},
+            { field: 'credit', header: 'Credit', type: Types.number, fixVal: 2,  class:'text-right'},
+            { field: 'rm', header: 'RM', type: Types.select },
+            { field: 'mobile', header: 'Mobile', type: Types.text },
+            { field: 'email', header: 'Email', type: Types.text },
+            { field: 'last_top_up', header: 'Last Top-up', type: Types.dateTime, dateFormat: 'dd-MM-yyyy HH:mm:ss' }
+        ];
+        this.cols.unshift(...this.selectedColumns);
+        this.exportCol = cloneDeep(this.cols);
     }
 
     ngOnInit(): void {
@@ -116,6 +136,7 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
             }
             this.primengTable['filters'] = resp['table_config'];
             this.isFilterShow = true;
+            this.selectedColumns = this.checkSelectedColumn(resp['selectedColumns'] || [], this.selectedColumns);
             this.primengTable._filter();
         });
     }
@@ -142,8 +163,37 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
             // this.primengTable['_sortField'] = filterData['sortColumn'];
             // this.sortColumn = filterData['sortColumn'];
             this.primengTable['filters'] = filterData['table_config'];
+            this.selectedColumns = this.checkSelectedColumn(filterData['selectedColumns'] || [], this.selectedColumns);
+            this.onColumnsChange();
+        } else {
+            this.selectedColumns = this.checkSelectedColumn([], this.selectedColumns);
+            this.onColumnsChange();
         }
     }
+
+    onColumnsChange(): void {
+        this._filterService.setSelectedColumns({ name: this.filter_table_name, columns: this.selectedColumns });
+    }
+
+    checkSelectedColumn(col: any[], oldCol: Column[]): any[] {
+        if (col.length) return col;
+        else {
+            var Col = this._filterService.getSelectedColumns({ name: this.filter_table_name })?.columns || [];
+            if (!Col.length)
+                return oldCol;
+            else
+                return Col;
+        }
+    }
+
+    isDisplayHashCol(): boolean {
+        return this.selectedColumns.length > 0;
+    }
+
+    onSelectedColumnsChange(): void {
+        this._filterService.setSelectedColumns({ name: this.filter_table_name, columns: this.selectedColumns });
+    }
+
 
     getAgent(value: string) {
         this.agentService.getAgentComboMaster(value, true).subscribe((data) => {
@@ -188,7 +238,7 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
 
         req.skip = 0;
         req.take = this.totalRecords;
-        
+
         this.agentBalanceService.getWalletReportList(req).subscribe(data => {
             for (var dt of data.data) {
                 dt.last_top_up = dt.last_top_up ? DateTime.fromISO(dt.last_top_up).toFormat('dd-MM-yyyy hh:mm a') : '';
@@ -217,5 +267,16 @@ export class AgentBalanceComponent extends BaseListingComponent implements OnDes
             this._filterService.activeFiltData = {};
         }
     }
+
+    displayColCount(): number {
+        return this.selectedColumns.length + 1;
+    }
+
+
+    isValidDate(value: any): boolean {
+        const date = new Date(value);
+        return value && !isNaN(date.getTime());
+    }
+
 
 }
