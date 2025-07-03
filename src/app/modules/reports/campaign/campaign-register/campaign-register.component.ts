ca419e2a4ce8037de2e6af7ea0d18fec0966e22d
MultiiconTechnical9 <technical9.multiicon@gmail.com>
2025-07-03 15:16:58 +0530

    --Nahin | compaign register dynamic column changes done

--- src/app/modules/reports/campaign/campaign-register/campaign-register.component.ts
+++ src/app/modules/reports/campaign/campaign-register/campaign-register.component.ts
@@ -14,7 +14,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterOutlet } from '@angular/router';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
-import { BaseListingComponent, Column } from 'app/form-models/base-listing';
+import { BaseListingComponent, Column, Types } from 'app/form-models/base-listing';
 import { filter_module_name, messages, module_name, Security } from 'app/security';
 import { DateTime } from 'luxon';
 import { Excel } from 'app/utils/export/excel';
@@ -24,6 +24,7 @@ import { Subject, Subscription, takeUntil } from 'rxjs';
 import { CampaignRegisterService } from 'app/services/campaign-register.service';
 import { RefferralService } from 'app/services/referral.service';
 import { LuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { cloneDeep } from 'lodash';
 
 export const MY_FORMATS = {
   parse: {
@@ -92,6 +93,21 @@ export class CampaignRegisterComponent extends BaseListingComponent {
   startDate = new FormControl(DateTime.now().startOf('month').minus({ months: 3 }).toJSDate());
   endDate = new FormControl(DateTime.now().startOf('month').toJSDate());
 
+  index = {
+    leads: -1,
+    signUp: -1,
+    turnover: -1,
+    techGP: -1,
+    travelGP: -1,
+    totalGP: -1,
+  };
+
+  types = Types;
+  selectedColumns: Column[] = [];
+  exportCol: Column[] = [];
+  activeFiltData: any = {};
+  cols: Column[] = [];
+
   constructor(
     private campaignRegisterService: CampaignRegisterService,
     public _filterService: CommonFilterService,
@@ -101,6 +117,28 @@ export class CampaignRegisterComponent extends BaseListingComponent {
     this.sortColumn = 'campaignName';
     this.Mainmodule = this;
     this._filterService.applyDefaultFilter(this.filter_table_name);
+
+    this.selectedColumns = [
+      { field: 'campaignName', header: 'Name' , type:Types.text  },
+      { field: 'campaignCategory', header: 'Category', type:Types.text },
+      { field: 'campaignType', header: 'Type', type:Types.text },
+      { field: 'referralCode', header: 'Code', type:Types.text },
+      { field: 'rm', header: 'RM', type:Types.select },
+      { field: 'totalSpent', header: 'Total Spent', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'leadCost', header: 'Lead Cost', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'cac', header: 'CAC', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'cRatio', header: 'C. Ratio %' , type: Types.number, fixVal: 2, class: 'text-right'},
+      { field: 'advtRatio', header: 'Advt. Ratio', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'leads', header: 'Leads', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'signUp', header: 'Signup', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'turnover', header: 'Turnover', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'techGP', header: 'Tech GP', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'travelGP', header: 'Travel GP', type: Types.number, fixVal: 2, class: 'text-right' },
+      { field: 'totalGP', header: 'Total GP', type: Types.number, fixVal: 2, class: 'text-right' }
+    ];
+
+    this.cols.unshift(...this.selectedColumns);
+    this.exportCol = cloneDeep(this.cols);
   }
 
   isFilterShow: boolean = false;
@@ -116,6 +154,7 @@ export class CampaignRegisterComponent extends BaseListingComponent {
       // this.primengTable['_sortField'] = resp['sortColumn'];
       this.primengTable['filters'] = resp['table_config'];
       this.isFilterShow = true;
+      this.selectedColumns = this.checkSelectedColumn(resp['selectedColumns'] || [], this.selectedColumns);
       this.primengTable._filter();
     });
 
@@ -133,8 +172,63 @@ export class CampaignRegisterComponent extends BaseListingComponent {
       let filterData = JSON.parse(this._filterService.activeFiltData.grid_config);
       this.selectedRm = filterData['table_config']['rm']?.value;
       this.primengTable['filters'] = filterData['table_config'];
+      this.selectedColumns = this.checkSelectedColumn(filterData['selectedColumns'] || [], this.selectedColumns); //  add new
+      this.onColumnsChange(); //  add new
+    } else { //  add new
+      this.selectedColumns = this.checkSelectedColumn([], this.selectedColumns); //  add new
+      this.onColumnsChange(); //  add new
+    }
+    //this.getColIndex();
+  }
+
+  getColIndex(): void { //  add new
+    this.index.leads = this.selectedColumns.findIndex((item: any) => item.field == 'leads');
+    this.index.signUp = this.selectedColumns.findIndex((item: any) => item.field == 'signUp');
+    this.index.turnover = this.selectedColumns.findIndex((item: any) => item.field == 'turnover');
+    this.index.techGP = this.selectedColumns.findIndex((item: any) => item.field == 'techGP');
+    this.index.travelGP = this.selectedColumns.findIndex((item: any) => item.field == 'travelGP');
+    this.index.totalGP = this.selectedColumns.findIndex((item: any) => item.field == 'totalGP');
+
+  }
+
+  onColumnsChange(): void {
+    this._filterService.setSelectedColumns({ name: this.filter_table_name, columns: this.selectedColumns });
+  }
+
+  checkSelectedColumn(col: any[], oldCol: Column[]): any[] {
+    if (col.length) return col;
+    else {
+      var Col = this._filterService.getSelectedColumns({ name: this.filter_table_name })?.columns || [];
+      if (!Col.length)
+        return oldCol;
+      else
+        return Col;
     }
   }
+
+  isDisplayHashCol(): boolean {
+    return this.selectedColumns.length > 0;
+  }
+
+  onSelectedColumnsChange(): void {
+    this._filterService.setSelectedColumns({ name: this.filter_table_name, columns: this.selectedColumns });
+    this.getColIndex(); //  add new
+  }
+
+  isAnyIndexMatch(): boolean { //  add new
+    const len = this.selectedColumns?.length - 1;
+    return len == this.index.leads || len == this.index.signUp || len == this.index.turnover || len == this.index.techGP || len == this.index.travelGP || len == this.index.totalGP;
+
+  }
+
+  isDisplayFooter(): boolean { //  add new
+    return this.selectedColumns.some(x => x.field == 'leads' || x.field == 'signUp' || x.field == 'turnover' || x.field == 'techGP' || x.field == 'travelGP' || x.field == 'totalGP') ;
+  }
+
+  isNotDisplay(field: string): boolean { //  add new
+    return field != "leads" && field != "signUp" && field != "turnover" && field != "techGP" && field != "travelGP" && field != "totalGP"
+  }
+
 
 
   setMonthAndYear(normalizedMonthAndYear: any, datepicker: MatDatepicker<Date>) {
@@ -172,7 +266,7 @@ export class CampaignRegisterComponent extends BaseListingComponent {
   }
 
   refreshItems(event?: any): void {
-     const fromDate = this.startDate.value;
+    const fromDate = this.startDate.value;
     const toDate = this.endDate.value;
     const today = new Date();
 
@@ -190,7 +284,7 @@ export class CampaignRegisterComponent extends BaseListingComponent {
       this.alertService.showToast('error', 'To Date cannot be in the future.');
       return;
     }
-    
+
     this.isLoading = true;
 
     const request = this.getNewFilterReq(event);
@@ -270,5 +364,16 @@ export class CampaignRegisterComponent extends BaseListingComponent {
 
     this.destroy$.next(null);
     this.destroy$.complete();
+  }
+
+  displayColCount(): number {
+    return this.selectedColumns.length + 1;
+  }
+
+
+  isValidDate(value: any): boolean {
+    const date = new Date(value);
+    return value && !isNaN(date.getTime());
+
   }
 }
