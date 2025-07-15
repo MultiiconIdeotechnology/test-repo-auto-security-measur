ae38d2ffd52402ee7f58d81cff5cf942c3085784
technical20multiicon <technical20.multiicon@gmail.com>
2025-07-15 12:08:48 +0530

    jaydeep | airBlock tab , 2 fileds added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     // apiUrl: 'https://boapi.bontontech.com/api/',
-     apiUrl: 'https://localhost:60817/api/',
+     apiUrl: 'https://localhost:56452/api/',
     // apiUrl: 'http://localhost:48046/api/',
     // apiUrl: 'https://sandbox.api.bontonholidays.com/api/',
 
