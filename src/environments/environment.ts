d3d376800441b46249ba17038ba3642f0372935a
MI-Yash <tech2.multiicon@gmail.com>
2025-09-11 12:26:40 +0530

    df

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     // apiUrl: 'https://boapi.bontontech.com/api/',
-    //  apiUrl: 'https://localhost:56452/api/',
-    //  apiUrl: 'http://localhost:27400/api/',
-    apiUrl: 'https://developer.api.bontonholidays.com/api/',
+     apiUrl: 'https://localhost:52571/api/',
+    // apiUrl: 'http://localhost:48046/api/',
+    // apiUrl: 'https://uat.api.bontonholidays.com/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
     isEnvironment: 'staging'
