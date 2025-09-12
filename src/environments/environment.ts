1d669c8b040d520f21087e6af18a5242aeb44592
MultiiconTechnical10 <technical10.multiicon@gmail.com>
2025-09-12 18:56:39 +0530

    GM - changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://boapi.bontontech.com/api/',
+    // apiUrl: 'https://boapi.bontontech.com/api/',
     //  apiUrl: 'https://localhost:52571/api/',
-    // apiUrl: 'http://localhost:48046/api/',
+    apiUrl: 'https://localhost:44333/api/',
     //apiUrl: 'https://developer.api.bontonholidays.com/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
