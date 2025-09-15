e833392e9c323d8ac72fbf8d86492ae7ab71cf4e
MI-Yash <tech2.multiicon@gmail.com>
2025-09-15 09:50:42 +0530

    552

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     // apiUrl: 'https://boapi.bontontech.com/api/',
     //  apiUrl: 'https://localhost:52571/api/',
-    apiUrl: 'https://localhost:44333/api/',
+    apiUrl: 'https://localhost:52571/api/',
     //apiUrl: 'https://developer.api.bontonholidays.com/api/',
 
     stagingUrl: 'https://sandbox.partner.bontonholidays.com/',
